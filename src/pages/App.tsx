7a209c7fabd4cace3ed5abc206a4feb80fd5a738
invecoin-co <info@investoland.co>
2022-03-19 23:30:41 -0300

    New Background

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -38,21 +38,20 @@ const BodyWrapper = styled.div`
   overflow-x: hidden;
   z-index: 1;
   justify-content: center;
-  background-image: url('/images/group-pancake.svg');
+  background-image: url('/images/background1.png');
   background-repeat: no-repeat;
-  background-position: bottom 24px center;
+  background-position: center;
   background-size: 90%;
 
   ${({ theme }) => theme.mediaQueries.xs} {
-    background-size: auto;
+    background-size: 150%;
   }
 
   ${({ theme }) => theme.mediaQueries.lg} {
-    background-image: url('/images/arch-${({ theme }) => (theme.isDark ? 'dark' : 'light')}.svg'),
-      url('/images/left-pancake.svg'), url('/images/right-pancake.svg');
+    background-image: url('/images/background1.png');
     background-repeat: no-repeat;
-    background-position: center 420px, 10% 230px, 90% 230px;
-    background-size: contain, 266px, 266px;
+    background-position: center;
+    background-size: 150%;
     min-height: 90vh;
   }
 `
